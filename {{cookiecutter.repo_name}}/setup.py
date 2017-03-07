2e0e2cc304f62a49cb7d7e91c1973a2938252adf
Raphael Michel <mail@raphaelmichel.de>
2017-03-07 19:20:53 +0100

    Compile i18n files in setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,4 +1,7 @@
 import os
+from distutils.command.build import build
+
+from django.core import management
 from setuptools import setup, find_packages
 
 
@@ -9,6 +12,17 @@ except:
     long_description = ''
 
 
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1, interactive=False)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
 setup(
     name='{{cookiecutter.repo_name}}',
     version='1.0.0',
@@ -17,10 +31,12 @@ setup(
     url='{{cookiecutter.repo_url}}',
     author='{{cookiecutter.author_name}}',
     author_email='{{cookiecutter.author_email}}',
+    license='Apache Software License',
 
     install_requires=[],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
+    cmdclass=cmdclass,
     entry_points="""
 [pretix.plugin]
 {{cookiecutter.module_name}}={{cookiecutter.module_name}}:PretixPluginMeta
